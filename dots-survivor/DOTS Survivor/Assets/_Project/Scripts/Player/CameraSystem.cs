bb7493fb4ec2dd6ab6481520f9f2beccb6a131c5
Pepn <pepijn.thijssens@gmail.com>
2023-12-21 14:59:09 +0100

    Add new camera angles

--- dots-survivor/DOTS Survivor/Assets/_Project/Scripts/Player/CameraSystem.cs
+++ dots-survivor/DOTS Survivor/Assets/_Project/Scripts/Player/CameraSystem.cs
@@ -18,17 +18,26 @@ namespace DOTSSurvivor
         // This OnUpdate accesses managed objects and so cannot be Burst-compiled
         public void OnUpdate(ref SystemState state)
         {
-           if (Camera.main != null)
-           {
-               var transformLookup = SystemAPI.GetComponentLookup<LocalTransform>(true);
-               var controllerEntity = SystemAPI.GetSingletonEntity<Controller>();
-               var controllerTransform = transformLookup[controllerEntity];
-               var cameraTransform = Camera.main.transform;
+            if (Camera.main != null)
+            {
+                var transformLookup = SystemAPI.GetComponentLookup<LocalTransform>(true);
+                var controllerEntity = SystemAPI.GetSingletonEntity<Controller>();
+                var controllerTransform = transformLookup[controllerEntity];
+                var cameraTransform = Camera.main.transform;
            
-               var newCamPos = controllerTransform.Position + new float3(0, 0, -20);
-               // Set the camera position to the player's position
-               cameraTransform.position = math.lerp(cameraTransform.position, newCamPos, 0.3f);
-           }
+                //default
+                var newCamPos = controllerTransform.Position + new float3(0, 0, -20);
+                cameraTransform.rotation = new Quaternion(0,0,0,0);
+                //angled
+                //var newCamPos = controllerTransform.Position + new float3(4.11f, -5.17f, -11.36f);
+                //cameraTransform.rotation = new Quaternion(-0.293f, -0.0757f, 0.322f, 0.897f);
+
+                //third person
+                //var newCamPos = controllerTransform.Position + new float3(0, -2.2f, -3.3f);
+                //cameraTransform.rotation = new Quaternion(-0.437f, 0, 0, 0.899f);
+
+                cameraTransform.position = math.lerp(cameraTransform.position, newCamPos, 0.3f);
+            }
         }
     }
 }
